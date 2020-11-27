f2d19e55d92575d32040b56bd5fbe8c44a80839a
fakhriim <fakhriim@wizardsdev.com>
2020-11-27 13:07:49 +0400

    all first page ui and logic and route to second page and back is done

---
+++ src/main/java/beans/DateBean.java
@@ -0,0 +1,14 @@
+package beans;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateBean implements Serializable {
+    public String getCurrentTime() {
+        return new SimpleDateFormat("HH:mm z").format(new Date());
+    }
+    public String getCurrentDate() {
+        return new SimpleDateFormat("dd.MM.yyyy").format(new Date());
+    }
+}
